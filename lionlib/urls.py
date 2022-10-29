bd8f178d299cddc0d92434befad21913b1366768
ParkKyungWan <pkw8117197@gmail.com>
2022-10-29 22:53:51 +0900

    first commit

---
+++ lionlib/urls.py
@@ -0,0 +1,35 @@
+"""lionlib URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+import lionlib_app.views
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('', lionlib_app.views.index, name="index"),
+    path('login', lionlib_app.views.login, name="login"),
+    path('register', lionlib_app.views.register, name="register"),
+
+    path('trend', lionlib_app.views.trend, name="trend"),
+    path('my', lionlib_app.views.my, name="my"),
+    path('new', lionlib_app.views.new, name="new"),
+    
+    path('all', lionlib_app.views.all, name="all"),
+    path('show', lionlib_app.views.show, name="show"),
+
+]
---
+++ lionlib_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# index, login, register
+
+def index( request ):
+    return render( request, 'index/index.html');
+
+    
+def login( request ):
+    return render( request, 'index/login.html');
+
+    
+def register( request ):
+    return render( request, 'index/register.html');
+
+
+# trend, my, new
+
+def trend( request ):
+    return render( request, 'main/trend.html');
+
+    
+def my( request ):
+    return render( request, 'main/my.html');
+
+    
+def new( request ):
+    return render( request, 'main/new.html');
+
+#all books
+  
+def all( request ):
+    return render( request, 'main/all.html');
+
+#show post
+def show( request ):
+    return render( request, 'main/show.html');
+
+#posting
+def new( request ):
+    return render( request, 'main/new.html');
